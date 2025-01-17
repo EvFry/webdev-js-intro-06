78fff9b815b7ed7bf0d87f265fd52ad73ad82622
Evlen Fryling <evlen.fryling@gmail.com>
2025-01-17 18:10:17 -0500

    Taske 1 complete

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 // Prevent us from attempting to use variables
 // that are not declared
+/*
 "use strict"
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        break;
+    }
+    console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    if (i === 2) {
+        continue;
+    }
+    console.log(i);
+}
+
+let i = 0;
+while (i < 5) {
+    if (i === 4) {
+        break;
+    }
+    console.log(i);
+    i++;
+}
+*/
